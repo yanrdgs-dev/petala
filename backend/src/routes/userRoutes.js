8b9d15d32460f3993890af424d9a9532c3cdb01c
yanrdgs-dev <yansantosrodrigues.dev@gmail.com>
2024-11-27 14:36:32 -0300

    Adição da verificação de dados do cadastro

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,9 +1,26 @@
 const express = require("express");
+const { body } = require("express-validator");
 const router = express.Router();
 const userController = require("../controllers/userController");
 
 // Rota de cadastro
-router.post("/register", userController.register);
+router.post(
+  "/register",
+  [
+    body("name").trim().notEmpty().withMessage("O nome é obrigatório."),
+
+    body("email").isEmail().withMessage("Forneça um e-mail válido."),
+
+    body("password")
+      .isLength({ min: 8 })
+      .withMessage("A senha deve ter pelo menos 8 caracteres.")
+      .matches(/\d/)
+      .withMessage("A senha deve conter pelo menos um número.")
+      .matches(/[A-Za-z]/)
+      .withMessage("A senha deve conter pelo menos uma letra."),
+  ],
+  userController.register,
+);
 
 // Rota de Login (TODO)
 
