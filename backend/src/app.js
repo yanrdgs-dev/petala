473da39f382f176077c159b55aaa7ff93aabe526
yanrdgs-dev <yansantosrodrigues.dev@gmail.com>
2024-11-28 20:58:38 -0300

    Adição do CORS para requisição HTML

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,13 +3,15 @@ const bodyParser = require("body-parser");
 const userRoutes = require("./routes/userRoutes");
 const db = require("./config/db");
 const cors = require("cors");
+
 const app = express();
+
+app.use(cors());
 
 // Config para que o express entenda JSON no corpo da requisição
 app.use(bodyParser.json());
 
 // Requisição do html
-app.use(cors());
 
 // Roteamento
 app.use("/api/users", userRoutes);
