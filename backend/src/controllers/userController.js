c769c452104d833f3fd18c711918d7b417ca517d
yanrdgs-dev <yansantosrodrigues.dev@gmail.com>
2024-11-28 17:23:05 -0300

    Alteração para database estar com coluna de username

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -9,36 +9,50 @@ exports.register = (req, res) => {
     return res.status(400).json({ errors: errors.array() });
   }
 
-  const { name, email, password } = req.body;
+  const { username, name, email, password } = req.body;
 
   //Verifica se o email já existe
-  db.query("SELECT * FROM users WHERE email = ?", [email], (err, result) => {
-    if (err) {
-      return res.status(500).json({ message: "Erro ao verificar e-mail." });
-    }
-    if (result.length > 0) {
-      return res.status(400).json({ message: "E-mail já cadastrado." });
-    }
-
-    // Criptografar senha
-    bcrypt.hash(password, 10, (err, hashedPassword) => {
+  db.query(
+    "SELECT * FROM users WHERE email = ? OR username = ?",
+    [email, username],
+    (err, result) => {
       if (err) {
-        return res.status(500).json({ message: "Erro ao criptografar senha." });
+        return res.status(500).json({ message: "Erro ao verificar e-mail." });
       }
+      if (result.length > 0) {
+        if (result.some((user) => user.email === email)) {
+          return res.status(400).json({ message: "E-mail já cadastrado." });
+        }
+        if (result.some((user) => user.username === username)) {
+          return res
+            .status(400)
+            .json({ message: "Nome de usuário já existe." });
+        }
+      }
+      // Criptografar senha
+      bcrypt.hash(password, 10, (err, hashedPassword) => {
+        if (err) {
+          return res
+            .status(500)
+            .json({ message: "Erro ao criptografar senha." });
+        }
 
-      // Inserir usuário no banco de dados
-      db.query(
-        "INSERT INTO users (name, email, password_hash) VALUES (?, ?, ?)",
-        [name, email, hashedPassword],
-        (err) => {
-          if (err) {
-            return res
-              .status(500)
-              .json({ message: "Erro ao cadastrar usuário." });
-          }
-          res.status(201).json({ message: "Usuário cadastrado com sucesso." });
-        },
-      );
-    });
-  });
+        // Inserir usuário no banco de dados
+        db.query(
+          "INSERT INTO users (username, name, email, password_hash) VALUES (?, ?, ?, ?)",
+          [username, name, email, hashedPassword],
+          (err) => {
+            if (err) {
+              return res
+                .status(500)
+                .json({ message: "Erro ao cadastrar usuário." });
+            }
+            res
+              .status(201)
+              .json({ message: "Usuário cadastrado com sucesso." });
+          },
+        );
+      });
+    },
+  );
 };
