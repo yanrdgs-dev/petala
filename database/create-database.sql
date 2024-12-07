b52cc384779db8e150ad498ae1901cf3ac7fb2e9
Rodrigoatila09 <154346800+Rodrigoatila09@users.noreply.github.com>
2024-12-06 23:31:11 -0300

    novas colunas criadas para validação de email

--- database/create-database.sql
+++ database/create-database.sql
@@ -10,7 +10,10 @@ CREATE TABLE IF NOT EXISTS users (
     username VARCHAR(50) UNIQUE NOT NULL, -- username do usuário
     name VARCHAR(100) NOT NULL, -- nome do usuário
     email VARCHAR(100) UNIQUE NOT NULL, -- email único
-    password_hash VARCHAR(255) NOT NULL -- senha criptografada
+    password_hash VARCHAR(255) NOT NULL, -- senha criptografada
+    email_verification_token VARCHAR(255) NOT NULL,
+    email_verify BOOLEAN DEFAULT false,
+    email_expires TIMESTAMP 
 );
 
 -- Logs de acesso (TODO)
