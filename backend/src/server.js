1062e20122b39a36e6a9c98c8b4a9a5592f89137
LasagnaIsLove <gabriel1990mota@gmail.com>
2024-12-07 20:54:46 -0300

    adição dos parte de envio de email para a pasta server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,46 @@ const app = require("./app");
 
 const PORT = process.env.PORT || 3000;
 
+const express = require('express');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'petala.donotrespond@gmail.com',
+    pass: 'petalaroot'
+  }
+})
+const carregartemplate = (templatepath, substuicoes) => {
+  let html = fs.readFileSync(templatepath, 'utf8');
+  for (const [chave, valor] of Object.entries(substuicoes)) {
+    html = html.replace(`{{${chave}}}`, valor);
+  }
+  return html;
+}
+app.post('/api/enviar-email', async (req, res) => {
+  const { nome, email} = req.body;
+
+  try{
+    const templatepath = path.join(_dirname, 'frontend', 'templates', 'email_boas-vindas.html');
+    const html = carregartemplate(templatepath, { nome })
+  
+    const mailOptions = {
+      from: 'petala.donotrespond@gmail.com',
+      to: email,
+      subject: 'Bem vindo ao Pétala!',
+      html: html,
+    };
+
+    await transporter.sendMail(mailOptions);
+    res.status(200).send('E-mail enviado com sucesso!');
+  } catch (error) {
+    res.status(500).send('Error ao enviar E-mail')
+    console.error(error);
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
