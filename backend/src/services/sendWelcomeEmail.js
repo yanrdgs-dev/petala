9af4381cd41b1f51abcb9f1373c3e165475ac606
LasagnaIsLove <gabriel1990mota@gmail.com>
2024-12-14 09:38:39 -0300

    Alteração no aquirvo sendWelcomeEmail.js para suportar o envio de images

--- backend/src/services/sendWelcomeEmail.js
+++ backend/src/services/sendWelcomeEmail.js
@@ -1,8 +1,10 @@
 const sendEmail = require("../utils/mailer");
+const path = require("path");
 
 async function sendWelcomeEmail(user) {
   const variables = {
     name: user.name,
+    petala_logo: "cid:logo_petala_image",
   };
 
   await sendEmail(
@@ -10,6 +12,19 @@ async function sendWelcomeEmail(user) {
     "Bem vindo ao Pétala!",
     "../../frontend/templates/email_boas-vindas.html",
     variables,
+    [
+      {
+        filename: "petala_logo.png",
+        path: path.resolve(
+          __dirname,
+          "frontend",
+          "assets",
+          "images",
+          "petala_logo.png"
+        ),
+        cid: "logo_petala_image",
+      },
+    ]
   );
 }
 
