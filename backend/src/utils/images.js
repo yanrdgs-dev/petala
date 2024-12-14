6012a812acda30fcda33be70b8a82f239e169ce4
LasagnaIsLove <gabriel1990mota@gmail.com>
2024-12-14 10:50:59 -0300

    criação do arquivo images.js

---
+++ backend/src/utils/images.js
@@ -0,0 +1,17 @@
+const path = require("path");
+
+module.export = {
+  images: [
+    {
+      __filename: "petala_logo.png",
+      path: path.resolve(
+        __dirname,
+        "frontend",
+        "assets",
+        "images",
+        "petala_logo.png"
+      ),
+      cid: "logo_petala_image",
+    },
+  ],
+};
