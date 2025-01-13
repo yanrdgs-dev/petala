39bbe177acfc70c0a7dac57b3e3923cff6cf6ad0
yanrdgs-dev <yansantosrodrigues.dev@gmail.com>
2025-01-13 10:43:38 -0300

    fix: adição do prazo na tabela checklist

--- database/checklist_tabela.sql
+++ database/checklist_tabela.sql
@@ -8,6 +8,7 @@ CREATE TABLE checklist (
     titulo VARCHAR(255) NOT NULL,                   -- Título da tarefa
     descricao TEXT,                                 -- Descrição da tarefa
     status ENUM('pendente', 'concluida') DEFAULT 'pendente', -- Status da tarefa
+    prazo DATETIME,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- Data de criação
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, -- Data de atualização
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE -- Relacionamento com a tabela de usuários
