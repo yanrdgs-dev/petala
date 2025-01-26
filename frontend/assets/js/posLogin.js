50395eba819a12cfad31018e77a450d3b885568b
Rodrigoatila09 <154346800+Rodrigoatila09@users.noreply.github.com>
2025-01-26 15:10:11 -0300

    Coreção na lógica

--- frontend/assets/js/posLogin.js
+++ frontend/assets/js/posLogin.js
@@ -1,20 +1,19 @@
 const token = localStorage.getItem('token');
 const welcomeMessage = document.getElementById('welcomeMessage');
-const checklistButton = document.getElementById('checklistButton');
+const checklistButton = document.getElementById('btn-checklist');
 
 if (!token) {
   alert('Você precisa estar logado!');
   window.location.href = '/login.html';
 } else {
   // Decodificar o token para extrair o nome do usuário
-  const payload = JSON.parse(atob(token.split('.')[1])); // Decodifica a parte do payload do token
+  const payload = JSON.parse(atob(token.split('.')[1])); 
   const username = payload.username;
 
-  // Atualiza a mensagem de boas-vindas com o nome do usuário
+
   welcomeMessage.textContent = `Bem vindo, ${username}! Vamos estudar?`;
 }
 
-// Redireciona para a página de checklist
 checklistButton.addEventListener('click', () => {
   window.location.href = './CheckList.html';
 });
