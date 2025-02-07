81dbd54c531f458f3a5023b307739f97e716cbb8
matheus-06 <matheus2006.pinheiro@gmail.com>
2025-02-07 15:02:55 -0300

    .

--- frontend/assets/js/configuracao.js
+++
@@ -1,9 +0,0 @@
-function showpage(tabId) {
-    // Oculta todos os conteúdos
-    document.querySelectorAll('.tab-content').forEach((content) => {
-      content.classList.remove('active');
-    });
-  
-    // Mostra o conteúdo da aba selecionada
-    document.getElementById(tabId).classList.add('active');
-  }
