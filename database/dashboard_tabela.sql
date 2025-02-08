2fc7ee03db02da71f16d42fc2fce5c4df1ccfe62
yanrdgs-dev <yansantosrodrigues.dev@gmail.com>
2025-02-07 23:04:53 -0300

    fix: alterações na tabela do dashboard

---
+++ database/dashboard_tabela.sql
@@ -0,0 +1,18 @@
+-- Seleciona o banco de dados
+USE petala;
+
+-- Cria tabela de informações do dashboard
+CREATE TABLE dashboard (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    tarefas_em_progresso INT DEFAULT 0,
+    tarefas_completas_semana INT DEFAULT 0,
+    tempo_foco_semana TIME DEFAULT 0, -- armazenado em minutos
+    provas_futuras INT DEFAULT 0,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+-- Mensagem de sucesso
+SELECT "Tabela de informações do dashboard criada com sucesso.";
